989e98dea5a17f0ce4ea90804823e6a8b43b16b8
Tomer <dror.tomer.il@gmail.com>
2018-11-26 21:08:21 +0200

    1

---
+++ java/bgu/spl/mics/application/passiveObjects/Customer.java
@@ -0,0 +1,91 @@
+package bgu.spl.mics.application.passiveObjects;
+
+import java.util.List;
+
+/**
+ * Passive data-object representing a customer of the store.
+ * You must not alter any of the given public methods of this class.
+ * <p>
+ * You may add fields and methods to this class as you see fit (including public methods).
+ */
+public class Customer
+{
+    private int id; // int – The id number of the customer.
+    private String name; // String – The name of the customer.
+    private String address; // String – The address of the customer.
+    private int distance; // int – the distance of the customer’s address from the store.
+    private List<OrderReceipt> Receipts; // List – all the receipts issued to the customer.
+    private int creditCard; // int – The number of the credit card of the customer.
+    private int availableAmountInCreditCard; // The remaining available amount of money in the creditcardof the custome
+
+    /**
+     * Retrieves the name of the customer.
+     */
+    public String getName ()
+    {
+        // TODO Implement this
+        return name;
+    }
+
+    /**
+     * Retrieves the ID of the customer  .
+     */
+    public int getId ()
+    {
+        // TODO Implement this
+        return id;
+    }
+
+    /**
+     * Retrieves the address of the customer.
+     */
+    public String getAddress ()
+    {
+        // TODO Implement this
+        return address;
+    }
+
+    /**
+     * Retrieves the distance of the customer from the store.
+     */
+    public int getDistance ()
+    {
+        // TODO Implement this
+        return distance;
+    }
+
+
+    /**
+     * Retrieves a list of receipts for the purchases this customer has made.
+     * <p>
+     *
+     * @return A list of receipts.
+     */
+    public List<OrderReceipt> getCustomerReceiptList ()
+    {
+        // TODO Implement this
+        return Receipts;
+    }
+
+    /**
+     * Retrieves the amount of money left on this customers credit card.
+     * <p>
+     *
+     * @return Amount of money left.
+     */
+    public int getAvailableCreditAmount ()
+    {
+        // TODO Implement this
+        return availableAmountInCreditCard;
+    }
+
+    /**
+     * Retrieves this customers credit card serial number.
+     */
+    public int getCreditNumber ()
+    {
+        // TODO Implement this
+        return creditCard;
+    }
+
+}
---
+++ java/bgu/spl/mics/application/passiveObjects/OrderReceipt.java
@@ -0,0 +1,96 @@
+package bgu.spl.mics.application.passiveObjects;
+
+/**
+ * Passive data-object representing a receipt that should
+ * be sent to a customer after the completion of a BookOrderEvent.
+ * You must not alter any of the given public methods of this class.
+ * <p>
+ * You may add fields and methods to this class as you see fit (including public methods).
+ */
+public class OrderReceipt
+{
+    private int orderId; // int – the id of the order.
+    private String seller; // string - the name of the service which handled the order.
+    private int customer; // id - the id of the customer the receipt is issued to.
+    private String bookTitle; // string – title of the book bought.
+    private int price; // int – the price the customer paid for the book.
+    private int issuedTick; // int - tick in which this receipt was issued (upon completing the correspondingevent).
+    private int orderTick; // int - tick in which the customer ordered the book.
+    private int proccessTick; // int – tick in which the selling service started processing the order.
+
+    /**
+     * Retrieves the orderId of this receipt.
+     */
+    public int getOrderId ()
+    {
+        // TODO Implement this
+        return orderId;
+    }
+
+    /**
+     * Retrieves the name of the selling service which handled the order.
+     */
+    public String getSeller ()
+    {
+        // TODO Implement this
+        return seller;
+    }
+
+    /**
+     * Retrieves the ID of the customer to which this receipt is issued to.
+     * <p>
+     *
+     * @return the ID of the customer
+     */
+    public int getCustomerId ()
+    {
+        // TODO Implement this
+        return customer;
+    }
+
+    /**
+     * Retrieves the name of the book which was bought.
+     */
+    public String getBookTitle ()
+    {
+        // TODO Implement this
+        return bookTitle;
+    }
+
+    /**
+     * Retrieves the price the customer paid for the book.
+     */
+    public int getPrice ()
+    {
+        // TODO Implement this
+        return price;
+    }
+
+    /**
+     * Retrieves the tick in which this receipt was issued.
+     */
+    public int getIssuedTick ()
+    {
+        // TODO Implement this
+        return issuedTick;
+    }
+
+    /**
+     * Retrieves the tick in which the customer sent the purchase request.
+     */
+    public int getOrderTick ()
+    {
+        // TODO Implement this
+        return orderTick;
+    }
+
+    /**
+     * Retrieves the tick in which the treating selling service started
+     * processing the order.
+     */
+    public int getProcessTick ()
+    {
+        // TODO Implement this
+        return proccessTick;
+    }
+}
