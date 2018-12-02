d8138520d5d1c3da69b93127d865eeafb38b9447
Tomer <dror.tomer.il@gmail.com>
2018-12-02 14:19:11 +0200

    6

---
+++ java/bgu/spl/mics/application/passiveObjects/InventoryTest.java
@@ -0,0 +1,45 @@
+package bgu.spl.mics.application.passiveObjects;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class InventoryTest
+{
+    @Deployment
+    public static JavaArchive createDeployment ()
+    {
+        return ShrinkWrap.create( JavaArchive.class ).addClass( Inventory.class ).addAsManifestResource( EmptyAsset.INSTANCE, "beans.xml" );
+    }
+
+    @org.junit.Test
+    public void getInstance ()
+    {
+    }
+
+    @org.junit.Test
+    public void load ()
+    {
+    }
+
+    @org.junit.Test
+    public void take ()
+    {
+    }
+
+    @org.junit.Test
+    public void checkAvailabiltyAndGetPrice ()
+    {
+    }
+
+    @org.junit.Test
+    public void printInventoryToFile ()
+    {
+    }
+}
